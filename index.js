2d2c043adc2ea5351aa279ee11047e3fc961a46a
Marina <dubovik.m14@gmail.com>
2018-03-05 19:29:01 +0200

    create calculate function

---
+++ index.js
@@ -0,0 +1,45 @@
+const Calculator = {
+    calculate: (expression) => {
+        let stack = [];
+        let tokens = expression.split(' ');
+
+        let counter = 0;
+        let counter2 = 0;
+        for (let i = 0; i < tokens.length; i++) {
+            let token = tokens[i];
+
+            if (Calculator.isNumber(token)) {
+                stack.push(parseInt(token));
+            }
+            if (Calculator.isOperand(token)) {
+                stack.push(
+                    Calculator.doOperation(
+                        token,
+                        stack.pop(),
+                        stack.pop()
+                    )
+                );
+            }
+        }
+        return stack.pop();
+    },
+
+    doOperation: (operator, x, y) => {
+        return Calculator.operations[operator](x, y);
+    },
+
+    operations: {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y
+    },
+
+    isNumber: token => {
+        return /\d+/.test(token);
+    },
+
+    isOperand: token => {
+        return /[-\+\*\/]/.test(token);
+    }
+}
